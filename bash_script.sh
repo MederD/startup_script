f47ef0fe393a2c44c6d37752f47ad86ba63a2f52
MederD <freenetscaper@yahoo.com>
2021-09-29 13:38:41 -0700

    adding script file

--- bash_script.sh
+++ bash_script.sh
@@ -6,6 +6,6 @@ amazon-linux-extras install epel -y
 yum install java-1.8* -y
 find /usr/lib/jvm/java-1*-*64 | head -n 1 > ~/env.txt
 sed -i 's/^/JAVA_HOME/' ~/env.txt
-export $(xargs <test.txt)
+export $(xargs <~/env.txt)
 PATH=$PATH:$JAVA_HOME
 sed -i '/PATH=/ s/$/:$JAVA_HOME/' ~/.bash_profile

333f36390e0899d702a7e65a9733f6ef3086fc57
MederD <freenetscaper@yahoo.com>
2021-10-01 16:16:30 -0700

    rev02

--- bash_script.sh
+++ bash_script.sh
@@ -8,6 +8,8 @@ sed -i 's/^/JAVA_HOME=/' ~/env.txt
 export $(xargs <~/env.txt)
 PATH=$PATH:$JAVA_HOME
 sed -i '/PATH=/ s/$/:$JAVA_HOME/' ~/.bash_profile
+cat ~/env.txt >> ~/.bash_profile
+source ~./bash_profile
 #-----------------
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
@@ -16,12 +18,13 @@ sudo service jenkins start
 sudo chkconfig jenkins on
 cat /var/lib/jenkins/secrets/initialAdminPassword > ~/jenkins_initial_admin_pswd.txt
 #-------------------
-sudo yum install git -yum
+sudo yum install git -y
 #------------------------
 mkdir /opt/maven
 cd /opt/maven
-wget http://mirrors.estointernet.in/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
+wget https://dlcdn.apache.org/maven/maven-3/3.8.2/binaries/apache-maven-3.8.2-bin.tar.gz
 tar -xvzf apache-maven-3.6.1-bin.tar.gz
 sed -i -e '$aM2_HOME=/opt/maven/apache-maven-3.6.1' ~/.bash_profile
-sed -i -e 'M2=$M2_HOME/bin' ~/.bash_profile
+sed -i -e 'M2=\$M2_HOME/bin' ~/.bash_profile
 sed -i '/PATH=/ s/$/:$M2_HOME:$M2/' ~/.bash_profile
+source ~./bash_profile
